478cd360efeba0841c028af03720795d5ff2e0c0
Ludorg <ludorg@ludorg.net>
2022-03-10 22:28:49 +0000

    gitpod script on port 8080

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,6 @@
+echo "starting cyber-dojo"
 sudo mkdir /cyber-dojo
 sudo chown 19663:65533 /cyber-dojo
 curl -O https://raw.githubusercontent.com/cyber-dojo/commander/master/cyber-dojo
 chmod 700 cyber-dojo
-./cyber-dojo up
+./cyber-dojo up --port=8080
